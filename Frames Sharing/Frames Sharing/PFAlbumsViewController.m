6c96cd4e98ef3fe80c67ead158e5e4a99e675167
Terry Lewis <terrylewis@outlook.com>
2013-03-04 12:24:23 -0600

    Added collection view for explore view controller and table view for albums view controller. Also added custom cell class for explore collection view.

---
+++ Frames Sharing/Frames Sharing/PFAlbumsViewController.m
@@ -0,0 +1,23 @@
+//
+//  PFAlbumsViewController.m
+//  Frames Sharing
+//
+//  Created by Terry Lewis II on 3/4/13.
+//  Copyright (c) 2013 Blue Plover Productions. All rights reserved.
+//
+
+#import "PFAlbumsViewController.h"
+
+@interface PFAlbumsViewController ()
+
+@end
+
+@implementation PFAlbumsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end
---
+++ Frames Sharing/Frames Sharing/PFExploreViewController.m
@@ -0,0 +1,70 @@
+//
+//  PFExploreViewController.m
+//  Frames Sharing
+//
+//  Created by Terry Lewis II on 3/4/13.
+//  Copyright (c) 2013 Blue Plover Productions. All rights reserved.
+//
+
+#import "PFExploreViewController.h"
+#import "PFExploreCell.h"
+@interface PFExploreViewController ()
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@end
+
+@implementation PFExploreViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+#pragma mark - UICollectionViewDelegate
+
+-(void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
+    //TODO deselect item
+}
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+#pragma mark – UICollectionViewDelegateFlowLayout
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CGSize retVal = CGSizeMake(75, 75);
+   // retVal.height += 35;
+   // retVal.width += 35;
+    return  retVal;
+}
+- (UIEdgeInsets)collectionView:
+(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsMake(10, 10, 10, 10);
+}
+#pragma mark - UICollectionView Datasource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+ 
+    return 3;
+}
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 5;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    PFExploreCell *cell = [cv dequeueReusableCellWithReuseIdentifier:@"ExploreCell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor greenColor];
+
+    return cell;
+}
+//- (UICollectionReusableView *)collectionView: (UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+//    FlckrPhotoHeaderView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:
+//                                        UICollectionElementKindSectionHeader withReuseIdentifier:@"FlckrPhotoHeaderView" forIndexPath:indexPath];
+//    NSString *searchTerm = self.searches[indexPath.section];
+//    [headerView setSearchText:searchTerm];
+//    return headerView;
+//}
+@end
