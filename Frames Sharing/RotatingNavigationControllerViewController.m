01a861f5d6e71c575905d1e778fc2f1893490a07
Djmobile <djmobileapps@gmail.com>
2013-04-19 16:14:11 -0500

    Getting Ready To Submit.
    It seems to work fine.

---
+++ Frames Sharing/RotatingNavigationControllerViewController.m
@@ -0,0 +1,38 @@
+//
+//  RotatingNavigationControllerViewController.m
+//  Frames Sharing
+//
+//  Created by Janusz Chudzynski on 4/19/13.
+//  Copyright (c) 2013 Blue Plover Productions. All rights reserved.
+//
+
+#import "RotatingNavigationControllerViewController.h"
+
+@interface RotatingNavigationControllerViewController ()
+
+@end
+
+@implementation RotatingNavigationControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
